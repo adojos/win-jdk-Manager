6fe4c9939b1bfee88e6cc5094c9bb251445882e4
Tushar Sharma <testoxide@gmail.com>
2019-09-06 03:26:32 +0100

    switching batch files

--- batchFiles/JDK8.bat
+++ batchFiles/JDK8.bat
@@ -1,11 +1,11 @@
 
 @echo off
 echo Setting JAVA_HOME 8
-setx JAVA_HOME=C:\JDKs\jdk1.8.0_212 /m
+setx JAVA_HOME C:\JDKs\jdk1.8.0_212 /m
 echo Setting JRE_HOME 8
 setx JRE_HOME C:\JDKs\jre1.8.0_212 /m
 echo setting PATH For Java 8
-setx PATH=C:\JDKs\jdk1.8.0_212\bin;%PATH% /m
+setx PATH C:\JDKs\jdk1.8.0_212\bin;%PATH% /m
 echo Display java version
 java -version
 pause
